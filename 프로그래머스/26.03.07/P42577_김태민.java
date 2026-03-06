a14af4bb0d5abbdd243c0fcb8b74c84f501a2766
김태민 <xoals2025@naver.com>
2026-03-06 18:17:54 +0900

    feat: P42577 김태민

---
+++ 프로그래머스/26.03.07/P42577_김태민.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class P42577_김태민 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        int n = phone_book.length;
+        Arrays.sort(phone_book);
+        for(int i = 0; i<n-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                answer = false;
+            }
+        }
+        return answer;
+    }
+}
